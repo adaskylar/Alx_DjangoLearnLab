f8670aa09b300c3562fde8d1fa1d3accfa944f94
Adaobi Ibekwe <adaodinukwe@gmail.com>
2025-11-19 11:50:05 +0100

    chore: integrated Book model with Django admin and updated documentation

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # 1. Columns shown in the admin list view
+    list_display = ('title', 'author', 'publication_year')
+
+    # 2. Filters shown on the right-hand side
+    list_filter = ('publication_year', 'author')
+
+    # 3. Search box (top right)
+    search_fields = ('title', 'author')
+
+    # (Optional) default ordering
+    ordering = ('title',)
